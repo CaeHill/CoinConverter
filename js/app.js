4719f0511687efa39a1bffee9bfa134d3d4d4966
João Pedro <joao.phrosa@gmail.com>
2026-03-31 15:20:31 -0300

    arrumado funcionando

--- js/app.js
+++ js/app.js
@@ -1,24 +1,24 @@
-const valorbase = Number(document.getElementById("input"));
-const moedabase = document.getElementById("moedabase");
-const moedaconvertidaprincipal = document.getElementById("moedaconvertida");
-const valorconvertidoprincipal = document.getElementById("valorconvertido");
-const moedasconvertidas1 = document.getElementById("moedasconvertidas1");
-const moedasconvertidas2 = document.getElementById("moedasconvertidas2");
-const moedasconvertidas3 = document.getElementById("moedasconvertidas3");
-const moedasconvertidas4 = document.getElementById("moedasconvertidas4");
-const valorconvertido1 = document.getElementById("valorconvertidas1");
-const valorconvertido2 = document.getElementById("valorconvertidas2");
-const valorconvertido3 = document.getElementById("valorconvertidas3");
-const valorconvertido4 = document.getElementById("valorconvertidas4");
-const bandeirabase = document.getElementById("bandeirabase")
 
-const bnt = document.getElementById("btnConversar")
-addEventListener('click', convert)
+const moedabase = document.getElementById("moedaBase");
+const moedaconvertidaprincipal = document.getElementById("moedaConvertida");
+const valorconvertidoprincipal = document.getElementById("valorConvertido");
+const moedasconvertidas1 = document.getElementById("moedaConvertida1");
+const moedasconvertidas2 = document.getElementById("moedaConvertida2");
+const moedasconvertidas3 = document.getElementById("moedaConvertida3");
+const moedasconvertidas4 = document.getElementById("moedaConvertida4");
+const valorconvertido1 = document.getElementById("valorConvertido1");
+const valorconvertido2 = document.getElementById("valorConvertido2");
+const valorconvertido3 = document.getElementById("valorConvertido3");
+const valorconvertido4 = document.getElementById("valorConvertido4");
+const bandeirabase = document.getElementById("bandeiraBase")
+
+const btn = document.getElementById("btnConversao")
+btn.addEventListener('click', convert)
 
 function convert() {
+  const valorbase = Number(document.getElementById("valorBase").value);
   const moedabaseformat = moedabase.value.toLowerCase();
 
-  
   const conversaoprincipal = moedaconvertidaprincipal.value.toLowerCase();
     fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${moedabaseformat}.json`)
       .then(res => res.json())
@@ -57,35 +57,34 @@ function convert() {
     .then(data => {
       const resultado4 = valorbase * data[moedabaseformat][conversao4];
       valorconvertido4.innerText = Number(resultado4.toFixed(2));
-    });
-}
+  })
+  switch (moedabase.value) {
+    case "BRL": 
+      bandeirabase.innerHTML = `<img src="https://flagsapi.com/BR/flat/24.png"></img>`
+      break
+      
+      case "USD": 
+      bandeirabase.innerHTML = `<img src="https://flagsapi.com/US/flat/24.png"></img>`
+      break
+      
+      case "EUR": 
+      bandeirabase.innerHTML = `<img src="https://flagsapi.com/GB/flat/24.png"></img>`
+      break
+      
+      case "JPY": 
+      bandeirabase.innerHTML = `<img src="https://flagsapi.com/JP/flat/24.png"></img>`
+      break
+      
+      case "GBP": 
+      bandeirabase.innerHTML = `<img src="https://flagsapi.com/GB/flat/24.png"></img>`
+      break
 
-switch (moedabase.value) {
-  case "BRL": 
-    bandeirabase.innerHTML = `<img src="https://flagsapi.com/BR/flat/24.png"></img>`
-    break
-    
-    case "USD": 
-    bandeirabase.innerHTML = `<img src="https://flagsapi.com/US/flat/24.png"></img>`
-    break
-    
-    case "EUR": 
-    bandeirabase.innerHTML = `<img src="https://flagsapi.com/GB/flat/24.png"></img>`
-    break
-    
-    case "JPY": 
-    bandeirabase.innerHTML = `<img src="https://flagsapi.com/JP/flat/24.png"></img>`
-    break
-    
-    case "GBP": 
-    bandeirabase.innerHTML = `<img src="https://flagsapi.com/GB/flat/24.png"></img>`
-    break
+      case "CNY": 
+      bandeirabase.innerHTML = `<img src="https://flagsapi.com/CN/flat/24.png"></img>`
+      break
 
-    case "CNY": 
-    bandeirabase.innerHTML = `<img src="https://flagsapi.com/CN/flat/24.png"></img>`
-    break
+      default:
+      bandeirabase.innerHTML = `<img src="https://img.cdndsgni.com/preview/12329729.jpg"></img>`
+}}
 
-    default:
-    bandeirabase.innerHTML = <img src="https://img.cdndsgni.com/preview/12329729.jpg"></img>
-} 
 
