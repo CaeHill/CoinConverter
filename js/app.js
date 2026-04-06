41b9b8dffbb4b6ddf5d08b0817bef643a55c0eaf
João Pedro <joao.phrosa@gmail.com>
2026-04-06 15:02:45 -0300

    backend arrumado com bandeiras fixas, simbolos anexados nas moedas fixas

--- js/app.js
+++ js/app.js
@@ -16,194 +16,64 @@ const bandeira2 = document.getElementById("flag2")
 const bandeira3 = document.getElementById("flag3")
 const bandeira4 = document.getElementById("flag4")
 const bandeira5 = document.getElementById("flag5")
+const valorbaseformatado = document.getElementById("valorBaseTexto")
 
+bandeirabase.innerHTML = `<img src="https://flagsapi.com/BR/flat/48.png"></img>`
+bandeira1.innerHTML = `<img src="https://flagsapi.com/US/flat/48.png"></img>`
+bandeira2.innerHTML = `<img src="https://flagsapi.com/DE/flat/48.png"></img>`
+bandeira3.innerHTML = `<img src="https://flagsapi.com/JP/flat/48.png"></img>`
+bandeira4.innerHTML = `<img src="https://flagsapi.com/GB/flat/48.png"></img>`
+bandeira5.innerHTML = `<img src="https://flagsapi.com/CN/flat/48.png"></img>`
 
 const btn = document.getElementById("btnConversao")
 btn.addEventListener('click', convert)
 
 function convert() {
-  const valorbase = Number(document.getElementById("valorBase").value);
-  const moedabaseformat = moedabase.value.toLowerCase();
+  const input = document.getElementById("valorBase");
+  let valorbase = Number(input.value.replace(/[^\d.-]/g, ""));
+  input.value = "R$ " + valorbase.toFixed(2);
+  const moedabaseformat = "brl";
 
-  const conversaoprincipal = moedaconvertidaprincipal.value.toLowerCase();
+  const conversaoprincipal = "usd";
     fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${moedabaseformat}.json`)
       .then(res => res.json())
       .then(data => {
         const resultado0 = valorbase * data[moedabaseformat][conversaoprincipal];
-        valorconvertidoprincipal.innerText = Number(resultado0.toFixed(2));
+        valorconvertidoprincipal.innerText = "$ " + Number(resultado0.toFixed(2));
       });
 
-  const conversao1 = moedasconvertidas1.value.toLowerCase();
+  const conversao1 = "eur";
   fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${moedabaseformat}.json`)
     .then(res => res.json())
     .then(data => {
       const resultado1 = valorbase * data[moedabaseformat][conversao1];
-      valorconvertido1.innerText = Number(resultado1.toFixed(2));
+      valorconvertido1.innerText = "€ " + Number(resultado1.toFixed(2));
     });
 
-  const conversao2 = moedasconvertidas2.value.toLowerCase();
+  const conversao2 = "jpy";
   fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${moedabaseformat}.json`)
     .then(res => res.json())
     .then(data => {
       const resultado2 = valorbase * data[moedabaseformat][conversao2];
-      valorconvertido2.innerText = Number(resultado2.toFixed(2));
+      valorconvertido2.innerText = "¥ " +Number(resultado2.toFixed(2));
     });
 
-  const conversao3 = moedasconvertidas3.value.toLowerCase();
+  const conversao3 = "gbp";
   fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${moedabaseformat}.json`)
     .then(res => res.json())
     .then(data => {
       const resultado3 = valorbase * data[moedabaseformat][conversao3];
-      valorconvertido3.innerText = Number(resultado3.toFixed(2));
+      valorconvertido3.innerText = "£ " +Number(resultado3.toFixed(2));
     });
 
-  const conversao4 = moedasconvertidas4.value.toLowerCase();
+  const conversao4 = "cny";
   fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${moedabaseformat}.json`)
     .then(res => res.json())
     .then(data => {
       const resultado4 = valorbase * data[moedabaseformat][conversao4];
-      valorconvertido4.innerText = Number(resultado4.toFixed(2));
+      valorconvertido4.innerText = "¥ " +Number(resultado4.toFixed(2));
+  
   })
-  switch (moedabase.value) {
-    case "BRL": 
-      bandeirabase.innerHTML = `<img src="https://flagsapi.com/BR/flat/24.png"></img>`
-      break
-      
-      case "USD": 
-      bandeirabase.innerHTML = `<img src="https://flagsapi.com/US/flat/48.png"></img>`
-      break
-      
-      case "EUR": 
-      bandeirabase.innerHTML = `<img src="https://flagsapi.com/GB/flat/48.png"></img>`
-      break
-      
-      case "JPY": 
-      bandeirabase.innerHTML = `<img src="https://flagsapi.com/JP/flat/48.png"></img>`
-      break
-      
-      case "GBP": 
-      bandeirabase.innerHTML = `<img src="https://flagsapi.com/GB/flat/48.png"></img>`
-      break
-
-      case "CNY": 
-      bandeirabase.innerHTML = `<img src="https://flagsapi.com/CN/flat/48.png"></img>`
-      break
-
-      default:
-      bandeirabase.innerHTML = `<img src="https://img.cdndsgni.com/preview/12329729.jpg"></img>`
   } 
-   switch (moedaconvertidaprincipal.value) {
-    case "BRL": 
-      bandeira1.innerHTML = `<img src="https://flagsapi.com/BR/flat/24.png"></img>`
-      break
-      
-      case "USD": 
-      bandeira1.innerHTML = `<img src="https://flagsapi.com/US/flat/48.png"></img>`
-      break
-      
-      case "EUR": 
-      bandeira1.innerHTML = `<img src="https://flagsapi.com/GB/flat/48.png"></img>`
-      break
-      
-      case "JPY": 
-      bandeira1.innerHTML = `<img src="https://flagsapi.com/JP/flat/48.png"></img>`
-      break
-      
-      case "GBP": 
-      bandeira1.innerHTML = `<img src="https://flagsapi.com/GB/flat/48.png"></img>`
-      break
 
-      case "CNY": 
-      bandeira1.innerHTML = `<img src="https://flagsapi.com/CN/flat/48.png"></img>`
-      break
-
-      default:
-      bandeira1.innerHTML = `<img src="https://img.cdndsgni.com/preview/12329729.jpg"></img>`
-  } 
-   switch (moedasconvertidas1.value) {
-    case "BRL": 
-      bandeira2.innerHTML = `<img src="https://flagsapi.com/BR/flat/24.png"></img>`
-      break
-      
-      case "USD": 
-      bandeira2.innerHTML = `<img src="https://flagsapi.com/US/flat/48.png"></img>`
-      break
-      
-      case "EUR": 
-      bandeira2.innerHTML = `<img src="https://flagsapi.com/GB/flat/48.png"></img>`
-      break
-      
-      case "JPY": 
-      bandeira2.innerHTML = `<img src="https://flagsapi.com/JP/flat/48.png"></img>`
-      break
-      
-      case "GBP": 
-      bandeira2.innerHTML = `<img src="https://flagsapi.com/GB/flat/48.png"></img>`
-      break
-
-      case "CNY": 
-      bandeira2.innerHTML = `<img src="https://flagsapi.com/CN/flat/48.png"></img>`
-      break
-
-      default:
-      bandeira2.innerHTML = `<img src="https://img.cdndsgni.com/preview/12329729.jpg"></img>`
-  } 
-   switch (moedasconvertidas2.value) {
-    case "BRL": 
-      bandeira3.innerHTML = `<img src="https://flagsapi.com/BR/flat/24.png"></img>`
-      break
-      
-      case "USD": 
-      bandeira3.innerHTML = `<img src="https://flagsapi.com/US/flat/48.png"></img>`
-      break
-      
-      case "EUR": 
-      bandeira3.innerHTML = `<img src="https://flagsapi.com/GB/flat/48.png"></img>`
-      break
-      
-      case "JPY": 
-      bandeira3.innerHTML = `<img src="https://flagsapi.com/JP/flat/48.png"></img>`
-      break
-      
-      case "GBP": 
-      bandeira3.innerHTML = `<img src="https://flagsapi.com/GB/flat/48.png"></img>`
-      break
-
-      case "CNY": 
-      bandeira3.innerHTML = `<img src="https://flagsapi.com/CN/flat/48.png"></img>`
-      break
-
-      default:
-      bandeira3.innerHTML = `<img src="https://img.cdndsgni.com/preview/12329729.jpg"></img>`
-  } 
-   switch (moedasconvertidas3.value) {
-    case "BRL": 
-      bandeira4.innerHTML = `<img src="https://flagsapi.com/BR/flat/24.png"></img>`
-      break
-      
-      case "USD": 
-      bandeira4.innerHTML = `<img src="https://flagsapi.com/US/flat/48.png"></img>`
-      break
-      
-      case "EUR": 
-      bandeira4.innerHTML = `<img src="https://flagsapi.com/GB/flat/48.png"></img>`
-      break
-      
-      case "JPY": 
-      bandeira4.innerHTML = `<img src="https://flagsapi.com/JP/flat/48.png"></img>`
-      break
-      
-      case "GBP": 
-      bandeira4.innerHTML = `<img src="https://flagsapi.com/GB/flat/48.png"></img>`
-      break
-
-      case "CNY": 
-      bandeira4.innerHTML = `<img src="https://flagsapi.com/CN/flat/48.png"></img>`
-      break
-
-      default:
-      bandeira4.innerHTML = `<img src="https://img.cdndsgni.com/preview/12329729.jpg"></img>`
-  } 
-}
-
-
+  
